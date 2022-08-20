a2cd337ead79c1473380795b47a8d6c9aa7a5383
Natabagass <meganatabagas@gmail.com>
2022-08-20 18:44:11 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat ducimus amet ipsum, ullam nemo delectus tempora nisi minus, cupiditate necessitatibus mollitia distinctio veritatis ipsam, adipisci expedita deserunt recusandae itaque perferendis.</p>
+    <ul>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis neque, deserunt vel aspernatur suscipit soluta, hic commodi delectus ex laborum et? Eum facere nihil a doloribus numquam ex facilis explicabo.</li>
+    </ul>
 </body>
 </html>
